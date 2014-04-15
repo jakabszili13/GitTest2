c28edf4bb8eafff0fa2360cc7686ae47483fbf5e
jakabszili13 <jakabszili15@gmail.com>
2014-04-15 03:59:08 +0200

    commit2
    cc

--- src/main.java
+++ src/main.java
@@ -1,7 +1,7 @@
 
 public class main {
 
-	public static void main(String[] args) {
+	public main(final String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Hello World!");
 		System.out.println("Hello World!");System.out.println("Hello World!");System.out.println("Hello World!");
